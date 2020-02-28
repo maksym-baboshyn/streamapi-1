f26022110afe57b0195385c7ca0e32b96209b727
yehor.pytomets <ujhvfrcbnrtjhg45*&#!njlfqusn[f>
2020-02-28 16:52:37 +0200

    Fix fails of Stream API Solution

--- src/main/java/baboshyn/stramapi/CliGenerator.java
+++ src/main/java/baboshyn/stramapi/CliGenerator.java
@@ -1,21 +1,17 @@
 package baboshyn.stramapi;
 
-import com.google.common.base.Joiner;
 import org.hamcrest.Matcher;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import static baboshyn.stramapi.CliGenerator.MatcherDescription.of;
 import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Stream.of;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public class CliGenerator {
-    private final Joiner joiner = Joiner.on("\n");
 
     /**
      * Checks that {@code commandBlocks} appear in the {@code actualCommands} in the order they were passed.
@@ -24,78 +20,46 @@ public class CliGenerator {
      * checked preserving the order.
      *
      * @param actualCommands source list of commands
-     * @param commandBlocks blocks of commands to appear in the source list
+     * @param commandBlocks  blocks of commands to appear in the source list
      */
     void assertCliCommandsContainBlocks(List<String> actualCommands, CommandBlock... commandBlocks) {
-//        List<String> actualCopy = new ArrayList<>(actualCommands);
 
-        final int[] index = {0};
-
-        final List<String> actualCopy = actualCommands
+        final List<String> actualCommandsFlattened = actualCommands
                 .stream()
-                .map(command -> Arrays.asList(command.split("\n")))
+                .map(command -> asList(command.split("\n")))
                 .flatMap(Collection::stream)
                 .collect(toList());
 
-        System.out.println(actualCopy);
-
-        boolean allMatch = Arrays.stream(commandBlocks)
-                .map(commandBlock -> of(convertCommandBlockToMatcher(commandBlock), commandBlock.commands.size()))
-                .peek(x -> System.out.println(x.matcher))
-                .allMatch(description -> {
-                    boolean matches = description.matcher.matches(actualCopy.subList(index[0], index[0] + description.size));
-                    index[0] = description.size;
-                    return matches;
-                });
-
-        System.out.println(allMatch);
+        boolean allMatch = stream(commandBlocks)
+                .allMatch(commandBlock -> findMatch(actualCommandsFlattened, commandBlock));
 
         if (!allMatch) {
 
             throw new AssertionError("Not matched.");
         }
+    }
 
- /*       if (!(isCommandsMatch && actualCopy.isEmpty())) {
+    private boolean findMatch(List<String> actualCommands, CommandBlock providedCommands) {
 
-            throw new AssertionError("Not matched.");
-        }*/
+        String command =
+                actualCommands.stream()
+                        .filter(providedCommands.commands::contains)
+                        .findFirst()
+                        .orElseGet(() -> {
+                            throw new AssertionError("Not matched.");
+                        });
 
-/*        for (CommandBlock commandBlock : commandBlocks) {
+        int commandIndex = actualCommands.indexOf(command);
 
-            final boolean fitsRemaining = commandBlock.commands.size() < actualCopy.size();
+        List<String> subList = actualCommands.subList(commandIndex, commandIndex + providedCommands.commands.size());
 
-//            final List<String> subList = actualCopy.subList(0, fitsRemaining ? commandBlock.commands.size() : actualCopy.size());
+        Matcher<Iterable<?>> matcher = convertCommandBlockToMatcher(providedCommands);
 
-            final long maxSize = fitsRemaining ? commandBlock.commands.size() : actualCopy.size();
+        boolean matches = matcher.matches(subList);
 
-            final List<String> subList = actualCopy.stream().limit(maxSize).collect(toList());
+        actualCommands.subList(0, commandIndex + providedCommands.commands.size()).clear();
 
-            final Object[] expectedBlock = joinMainAndSubCommands(commandBlock.commands).toArray();
-
-            final Matcher<Iterable<?>> matcher =
-                    commandBlock.checkOrder ? contains(expectedBlock) : containsInAnyOrder(expectedBlock);
-
-            int index = subList.size();
-
-            while (!matcher.matches(joinMainAndSubCommands(subList))) {
-
-                final String nextCommand = actualCopy.get(index);
-
-                subList.remove(0);
-
-                subList.add(nextCommand);
-
-                index++;
-
-                if (index == actualCopy.size()) {
-
-                    // we checked all remaining commands and the are no matches
-                    throw new AssertionError("Not matched: " + commandBlock.commands.toString());
-                }
-            }
-
-            actualCopy = fitsRemaining ? actualCopy.subList(index, actualCopy.size()) : emptyList();
-        }*/
+        return matches;
     }
 
     private Matcher<Iterable<?>> convertCommandBlockToMatcher(CommandBlock block) {
@@ -103,22 +67,6 @@ public class CliGenerator {
         return block.checkOrder ?
                 contains(block.commands.toArray()) :
                 containsInAnyOrder(block.commands.toArray());
-    }
-
-    static final class MatcherDescription {
-
-        private final Matcher<Iterable<?>> matcher;
-        private final int size;
-
-        private MatcherDescription(Matcher<Iterable<?>> matcher, int size) {
-            this.matcher = matcher;
-            this.size = size;
-        }
-
-        public static MatcherDescription of(Matcher<Iterable<?>> matcher, int size) {
-
-            return new MatcherDescription(matcher, size);
-        }
     }
 
     /**
@@ -142,24 +90,4 @@ public class CliGenerator {
             return new CommandBlock(false, commands);
         }
     }
-
-    /*
-    List<String> joinMainAndSubCommands(List<String> commands) {
-        final List<String> resultCommands = new ArrayList<>();
-
-        commands.forEach(nextCommand -> {
-            if (nextCommand.startsWith(SPACE)) {
-                final String prevCmd = resultCommands.isEmpty() ? null : resultCommands.remove(resultCommands.size() - 1);
-                if (prevCmd == null) {
-                    resultCommands.add(nextCommand);
-                } else {
-                    resultCommands.add(joiner.join(prevCmd, nextCommand));
-                }
-            } else {
-                resultCommands.add(nextCommand);
-            }
-        });
-        return resultCommands;
-    }
-    */
 }
--- src/test/java/baboshyn/stramapi/CliGeneratorTest.java
+++ src/test/java/baboshyn/stramapi/CliGeneratorTest.java
@@ -1,6 +1,7 @@
 package baboshyn.stramapi;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -27,46 +28,70 @@ class CliGeneratorTest {
         return Stream.of(
 
                 of(asList("a", "b", "c"), new CommandBlock[]{checkOrder("a", "b"), checkOrder("c")},
-                        "assert checking order command blocks"),
+                        "2 checking order blocks"),
 
                 of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("a", "b"), ignoreOrder("c")},
-                        "assert ignoring order command blocks"),
+                        "2 ignoring order blocks, commands of each going in right order"),
 
                 of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("b", "a"), ignoreOrder("c")},
-                        "assert ignoring order command blocks"),
+                        "2 ignoring order blocks, commands of each going in wrong order"),
 
-                of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("b", "a", "c")},
-                        "assert ignore order command blocks"),
+                of(asList("a", "b"), new CommandBlock[]{ignoreOrder("b", "a")},
+                        "1 ignoring order block, with size matching to the size fo actual block"),
 
-                of(asList("a", "b", " c"), new CommandBlock[]{ignoreOrder("a", "b", " c")},
-                        "assert ignore order command blocks"),
+                of(singletonList("a\n b"), new CommandBlock[]{ignoreOrder("a", " b")},
+                        "command with space in the middle of command block"),
 
                 of(asList("a\n b\n c", "d"), new CommandBlock[]{ignoreOrder("a", " b", " c", "d")},
-                        "assert ignore order command blocks"),
+                        "multiple commands with spaces"),
+
+                of(asList("a\n b", "c"), new CommandBlock[]{ignoreOrder("a"), ignoreOrder(" b", "c")},
+                        "command with space in the beginning of second block"),
+
+                of(asList("a", "b\n c"), new CommandBlock[]{checkOrder("a"), ignoreOrder(" c", "b")},
+                        "command both having space in the beginning of second block and ignoring order"),
 
                 of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("a", "b")},
-                        "assert ignore order command blocks"),
-
-                of(asList("a", "b", "c"), new CommandBlock[]{checkOrder("a"), checkOrder("b", "c")},
-                        "assert ignore order command blocks"),
+                        "command block included in actual block of commands"),
 
                 of(asList("a", "b", "c"), new CommandBlock[]{checkOrder("a"), ignoreOrder("c", "b")},
-                        "assert ignore order command blocks"),
+                        "mixed types of commands blocks"),
 
-                of(asList("a", "b", "c", "d", "e"),
+                of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("c", "b")},
+                        "case when first command missing and order ignored"),
+
+                of(asList("a", "b", "c", "d", "e", "f\n g", "k"),
                         new CommandBlock[]{
-                                ignoreOrder("b", "a"), ignoreOrder("d", "c"), checkOrder("e")},
-                        "assert ignore order command blocks")
+                                ignoreOrder("b", "a"),
+                                ignoreOrder("d", "c"),
+                                checkOrder("e"),
+                                ignoreOrder("k", "f", " g")},
+                        "multiple blocks ignoring order"),
+
+                of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("b", "c")},
+                        "case when first command missing and order is checked"),
+
+                of(asList("a", "b", "c", "d"), new CommandBlock[]{ignoreOrder("b", "c"), ignoreOrder("d")},
+                        "case with missing command and divided last commands"),
+
+                of(asList("a", "b", "c", "d"), new CommandBlock[]{ignoreOrder("b"), ignoreOrder("d")},
+                        "case when command missing through one"),
+
+                of(asList("a", "b", "c", "d"), new CommandBlock[]{ignoreOrder("b", "c", "d")},
+                        "case with missing command and joined last commands"),
+
+                of(asList("\n a", "b"), new CommandBlock[]{ignoreOrder(" a", "b")},
+                        "next-line (joiner) character found in the beginning of command line.")
         );
     }
 
     @ParameterizedTest
     @DisplayName("assert commands preserving order.")
     @MethodSource("testDataForPositiveScenarios")
-    void assertCommandsPreservingOrder(List<String> actualCommands, CommandBlock[] commandBlocks, String errorCause) {
+    void testCommandsPreservingOrder(List<String> actualCommands, CommandBlock[] commandBlocks, String errorCause) {
 
         assertDoesNotThrow(() -> generator.assertCliCommandsContainBlocks(actualCommands, commandBlocks),
-                "Asserting with " + errorCause + " failed.");
+                "Asserting of " + errorCause + " failed.");
     }
 
     private static Stream<Arguments> testDataForNegativeScenarios() {
@@ -74,40 +99,32 @@ class CliGeneratorTest {
         return Stream.of(
 
                 of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("b", "c"), ignoreOrder("a")},
-                        "assert ignoring order command blocks"),
-
-                of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("b", "c")},
-                        "assert ignore order command blocks"),
-
-                of(asList("a", "b", "c"), new CommandBlock[]{ignoreOrder("c", "b")},
-                        "assert ignore order command blocks"),
+                        "command blocks go in wrong order."),
 
                 of(singletonList("d"), new CommandBlock[]{ignoreOrder("d ")},
-                        "assert ignore order command blocks"),
+                        "command ends with space"),
 
                 of(singletonList("d "), new CommandBlock[]{ignoreOrder("d")},
-                        "assert ignore order command blocks"),
-
-                of(asList("a\n b", "c"), new CommandBlock[]{ignoreOrder("a"), ignoreOrder(" b", "c")},
-                        "assert ignore order command blocks"),
-
-                of(asList("a", "b\n c"), new CommandBlock[]{checkOrder("a"), ignoreOrder(" c", "b")},
-                        "assert ignore order command blocks"),
-
-                of(asList("a", "b"), new CommandBlock[]{ignoreOrder("a", "b", "c")},
-                        "assert ignore order command blocks")
+                        "actual command ends with space")
         );
     }
 
     @ParameterizedTest
     @DisplayName("not assert commands, not preserving order")
     @MethodSource("testDataForNegativeScenarios")
-    void notAssertCommandsNotPreservingOrder(List<String> actualCommands,
-                                             CommandBlock[] commandBlocks,
-                                             String errorCause) {
+    void testCommandsNotPreservingOrder(List<String> actualCommands, CommandBlock[] commandBlocks, String errorCause) {
 
         assertThrows(AssertionError.class,
                 () -> generator.assertCliCommandsContainBlocks(actualCommands, commandBlocks),
                 "Cannot recognize case when " + errorCause);
+    }
+
+    @Test
+    @DisplayName("not assert lesser number of actual commands")
+    void testDifferentNumberOfCommands() {
+
+        assertThrows(IndexOutOfBoundsException.class,
+                () -> generator.assertCliCommandsContainBlocks(asList("a", "b"), ignoreOrder("a", "b", "c")),
+                "Cannot say that number of actual commands less than number of provided commands.");
     }
 }
